e0da5bc29df8e631a4ccac5d309724d3f4d8678c
abnormalconfig <abnormalconfig@gmail.com>
2026-02-09 14:41:12 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10586261
+    "zoneId": 10585164
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
